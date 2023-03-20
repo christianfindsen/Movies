2d97b455a5bd39e4ed759d1177cbb12fe3e2050f
Christian Findsen <cfindsen@toogoodtogo.com>
2023-03-20 22:26:48 +0100

    Moved API key to the local properties /w build config

--- app/src/main/java/com/example/trendingmovies/data/remote/MoviesApi.kt
+++ app/src/main/java/com/example/trendingmovies/data/remote/MoviesApi.kt
@@ -1,18 +1,20 @@
 package com.example.trendingmovies.data.remote
 
+import com.example.trendingmovies.BuildConfig
 import com.example.trendingmovies.data.model.Movie
 import com.example.trendingmovies.data.model.MovieListResponse
 import retrofit2.Response
+import retrofit2.http.Field
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface MoviesApi {
 
 
-    @GET("trending/movie/week?api_key=c55c980c13fd91ab460068b841516c54")
+    @GET("trending/movie/week?api_key=${BuildConfig.API_KEY}")
     suspend fun getTrendingMovies(): Response<MovieListResponse>
 
-    @GET("movie/{id}?api_key=c55c980c13fd91ab460068b841516c54")
+    @GET("movie/{id}?api_key=${BuildConfig.API_KEY}")
     suspend fun getMovieById(
         @Path(value = "id", encoded = true) id: Int): Response<Movie>
 }
